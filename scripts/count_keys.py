96d06b50d7e0cf2eac99032b9cbc58af7ee48405
Artem Saakov <asaakov@umich.edu>
2024-12-18 23:58:20 -0500

    add helper script to display/count specific keys

--- scripts/count_keys.py
+++ scripts/count_keys.py
@@ -4,8 +4,8 @@ from pathlib import Path
 import json
 
 directory = Path.cwd() / Path('collection-data/item-metadata')  # Replace with your directory
-key_to_check = 'rights'  # Replace with the key you want to check
-key_to_compare = 'rights_information'  # Replace with the key you want to compare
+key_to_check = 'medium'  # Replace with the key you want to check
+key_to_compare = 'medium'  # Replace with the key you want to compare
 
 custom_key_count = 0
 count = 0
@@ -16,15 +16,14 @@ for filepath in directory.glob('*.json'):
             data = json.load(file)
             data = data.get('item', data)
             count += 1
-            key1 = data.get(key_to_check, None)
+            key1 = data['item'].get(key_to_check, None)
             key2 = data.get(key_to_compare, None)
-            print(key1, key2)
-            if not key1:
-                custom_key_count += 1
-            else:
-                print(f"File {filepath.name} does not have '{key_to_check}' equal to '{key_to_compare}'")
+            with open('output.txt', 'a') as f:
+                f.write(f'{filepath.name}:\nlanguage: {key1}\nlanguage #2: {key2}\n')
+
         except json.JSONDecodeError:
             print(f"Error decoding JSON in file {filepath.name}")
 
 print(f"Number of files where '{key_to_check}' is the same as {key_to_compare}: {custom_key_count}")
 print(f"Total number of files checked: {count}")
+
